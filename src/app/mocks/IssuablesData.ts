ab7a52372aba483c5af597f639631ce7b654207e
Noa <noa3005@gmail.com>
2017-02-08 23:53:52 +0200

    get Expired Issuables

--- src/app/mocks/IssuablesData.ts
+++ src/app/mocks/IssuablesData.ts
@@ -1,7 +1,10 @@
 import {Issuable} from "../classes/issueable";
 import {TOOLS_ROOMS} from "./ToolsRoomData"
 
-export const ISSUABLES:Issuable[] = [
-  new Issuable(1, 'מברג 1', Issuable.TOOL_TYPE, null, TOOLS_ROOMS[0]),
-  new Issuable(2, 'מברג 2', Issuable.TOOL_TYPE, null, TOOLS_ROOMS[1])
+export let ISSUABLES:Issuable[] = [
+  new Issuable(1, 'מברג 1', Issuable.TOOL_TYPE, null, TOOLS_ROOMS[0], Date.now() - 1000*60*60*24),
+  new Issuable(2, 'מברג 2', Issuable.TOOL_TYPE, null, TOOLS_ROOMS[1], Date.now() - 1000*60*60*24),
+  new Issuable(3, 'מברג 3', Issuable.TOOL_TYPE, null, TOOLS_ROOMS[1], Date.now() + 1000*60*60*24*5),
+  new Issuable(4, 'מברג 4', Issuable.TOOL_TYPE, null, TOOLS_ROOMS[2], Date.now() + 1000*60*60*24*17),
+  new Issuable(5, 'מברג 5', Issuable.TOOL_TYPE, null, TOOLS_ROOMS[2], Date.now() + 1000*60*60*24*30)
 ];
