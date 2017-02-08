3a0b860d8eba296ace1c08969bde14c6af01314e
Snir <snirmordhy@gmail.com>
2017-02-08 14:39:54 +0200

    add mocking for ToolsRoom

---
+++ src/app/mocks/ToolsRoomData.ts
@@ -0,0 +1,11 @@
+import  { ToolsRoom } from "../modules/ToolsRoom"
+
+export const TOOLS_ROOMS: ToolsRoom[] = [
+  {id: 1122, name: 'חדר כלים כנף 1'},
+  {id: 2233, name:'חדר כלים בח"א 10'},
+  {id: 6585, name:'חדר כלים כנף 2'},
+  {id: 7809, name:'חדר כלים תל נוף דת"ק 3'},
+  {id: 2635, name:'חדר כלים תל נוף דת"ק 1'},
+  {id: 4496, name:'חדר כלים פלמחים דת"ק 6'},
+  {id: 7686, name:'חדר כלים בח"א 8'}];
+
