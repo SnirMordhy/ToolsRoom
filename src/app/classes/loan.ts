c547fe56dcdc18593a299d7ebfdd41dec2ee73f5
droruri <droruri@gmail.com>
2017-02-08 19:48:19 +0200

    creating the loans Service

--- src/app/classes/loan.ts
+++ src/app/classes/loan.ts
@@ -1,6 +1,16 @@
 export class Loan {
+  public static LOANER:string = 'loaner';
+  public static LOANEE:string = 'loanee';
+
   id:number;
   issuables:Array<number>;
   isApproved:boolean;
   type:string;
+
+  constructor(id:number, issuables:Array<number>, isApproved:boolean,type:string){
+    this.id = id;
+    this.issuables = issuables;
+    this.isApproved = isApproved;
+    this.type = type;
+  }
 }
--- src/app/providers/loans.service.spec.ts
+++ src/app/providers/loans.service.spec.ts
@@ -1,25 +1,49 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import {TestBed, async, inject, tick, fakeAsync} from '@angular/core/testing';
 import { LoansService } from './loans.service';
+import {MockBackend, MockConnection} from "@angular/http/testing";
+import {ReflectiveInjector} from "@angular/core";
+import {ConnectionBackend, RequestOptions, BaseRequestOptions, Http, Response, ResponseOptions} from "@angular/http";
+import {TOOLS_ROOMS} from "../mocks/ToolsRoomData";
+import {ISSUABLES} from "../mocks/IssuablesData";
+import {Issuable} from "../classes/issueable";
 
 describe('LoansService', () => {
+  let loansService:LoansService;
+  let backend:MockBackend;
+  let lastConnection:MockConnection;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [LoansService]
-    });
+    this.injector = ReflectiveInjector.resolveAndCreate([
+      {provide: ConnectionBackend, useClass: MockBackend},
+      {provide: RequestOptions, useClass: BaseRequestOptions},
+      Http,
+      LoansService]);
+
+    loansService = this.injector.get(LoansService);
+    backend = this.injector.get(ConnectionBackend) as MockBackend;
+    backend.connections.subscribe((connection: MockConnection) => lastConnection = connection);
   });
 
-  it('should ...', inject([LoansService], (service: LoansService) => {
-    expect(service).toBeTruthy();
+  it('should be able to loan one tool to another toolRoom',fakeAsync(()=>{
+    let tool:Issuable = ISSUABLES[0];
+    loansService.loan([tool], TOOLS_ROOMS[1].id);
+    lastConnection.mockRespond(new Response(new ResponseOptions({
+      body: 154,
+    })));
+    tick();
+    expect(loansService.externalLoans.length).toBe(1);
   }));
 
-  it('should be able to loan tool to another tool room', ()=>{
-    let loaner:Object = {id: 1, name: "ToolsRoom1"};
-    let loanee:Object = {id: 2, name: "ToolsRoom2"};
-    let issuable:Object = {id: 1, name: "Tool1", type: "Tool", tools: [], owner: loaner};
+  it('should be able to loan one tool to another toolRoom',fakeAsync(()=>{
+    let tool:Issuable = ISSUABLES[0];
+    loansService.loan([tool], TOOLS_ROOMS[1].id);
+    lastConnection.mockRespond(new Response(new ResponseOptions({
+      body: 154,
+    })));
+    tick();
+    expect(loansService.externalLoans.length).toBe(1);
+  }));
 
-    LoansService.loanIssuable(issuable, loanee);
-
-  });
 });
